f8af6a86a3581043385c279e67ef04f3ff6d4dd4
berycberyc <52874383+berycberyc@users.noreply.github.com>
2026-09-03 21:25:07 +0500

    яяя

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,27 @@ export default function RootLayout({
       {/* Google Search Console */}
       <meta name="google-site-verification" content="rmvYBT4SaAmKV1KI2rZyDvEGoOqlrDjBnWdbsjoQDMc" />
 
+      {/* Meta Pixel */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1543620777027992');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{display:"none"}}
+          src="https://www.facebook.com/tr?id=1543620777027992&ev=PageView&noscript=1"
+        />
+      </noscript>
+
       {/* Google Analytics */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-WHGVYW7YVG"
